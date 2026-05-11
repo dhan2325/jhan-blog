5685a9b67fc26a68b25e6f3bd950e0efa0f7e827
Daniel Han <79017120+dhan2325@users.noreply.github.com>
2026-05-11 17:02:24 -0400

    move resume to public, try again to remove favicon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
           <> <a href="/">Home</a> | </>
           <a href="/blog">Blog</a> | 
           <a href="https://github.com/dhan2325" target="_blank" rel="noopener noreferrer">GitHub</a> | 
-          <a href="jhan_resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
+          <a href="/jhan_resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
         </span>
       </p>
     </div>
